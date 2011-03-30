b61b374757717696895c473936dcc63d6878f0cf
Ted O'Meara <ted@intridea.com>
2011-03-30 19:40:05 -0400

    initial commit

---
+++ Classes/CogConnectAppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  CogConnectAppDelegate.h
+//  CogConnect
+//
+//  Created by Edward O'Meara on 3/29/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface CogConnectAppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CogConnect
+//
+//  Created by Edward O'Meara on 3/29/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"CogConnectAppDelegate");
+	[pool release];
+	return retVal;
+}
